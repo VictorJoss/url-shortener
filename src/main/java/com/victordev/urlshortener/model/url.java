4990b74e7c7c8e867f86e7e0eafb0b775c34be2d
VictorJoss <vramirezgarcia64@correo.unicordoba.edu.co>
2024-08-19 22:22:10 -0500

    add url entity

---
+++ src/main/java/com/victordev/urlshortener/model/url.java
@@ -0,0 +1,23 @@
+package com.victordev.urlshortener.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String originalUrl;
+    private String shortUrl;
+    private LocalDateTime expirationDate;
+}
